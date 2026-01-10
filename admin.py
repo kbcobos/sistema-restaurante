d58726dd3038820ff750492d9db2be5eb6b41497
K. B. Cobos <kcoboscaballero@gmail.com>
2026-01-10 00:54:03 -0300

    desarrollo para guardar datos y registro de admin

--- admin.py
+++ admin.py
@@ -36,6 +36,71 @@ def cargar_datos():
         print(f"Error al cargar datos: {e}")
         return False
 
+def guardar_datos():
+    try:
+        ruta_admins = os.path.join(os.path.dirname(__file__), "admins.json")
+        with open(ruta_admins, "w", encoding="utf-8") as f:
+            json.dump(admins, f, indent=4, ensure_ascii=False)
+
+            ruta_admins = os.path.join(os.path.dirname(__file__), "menu.json")
+            with open(ruta_menu, "w", encoding="utf-8") as f:
+                json.dump(menu_productos, f, indent=4, ensure_ascii=False)
+
+            ruta_pedidos = os.path.join(os.path.dirname(__file__), "pedidos.json")
+            with open(ruta_pedidos, "w", encoding="utf-8") as f:
+                json.dump(pedidos, f, indent=4, ensure_ascii=False)
+            
+            print("Datos guardados correctamente")
+            return True
+        
+    except Exception as e:
+        print(f"Error al guardar datos: {e}")
+        return False
+    
+def registrar_admin(usuario, contrasena, mail, nombre, apellido):
+
+    datos = [usuario, contrasena, mail, nombre, apellido]
+    if not validar_datos_no_nulos(datos):
+        print("ERROR: Todos los campos son obligatorios")
+        return False
+    
+    if not validar_mail(mail):
+        print("ERROR: El mail debe tener un dominio valido")
+        return False
+    
+    if not validar_contrasena(contrasena):
+        print("ERROR: La contraseña debe tener al menos 5 caracteres")
+        return False
+    
+    if not validar_solo_letras(nombre) or not validar_solo_letras(apellido):
+        print("ERROR: Nombre y apellido solo pueden contener letras")
+        return False
+    
+    usuario = usuario.strip().lower()
+
+    if usuario in admins:
+        print(f"El administrador '{usuario}' ya existe")
+        return False
+    
+    try:
+        admins[usuario] = {
+            "Contraseña": contrasena.strip(),
+            "Mail": mail.strip().lower(),
+            "Nombre": nombre.strip(),
+            "Apellido": apellido.strip()
+        }
+
+        ruta = os.path.join(os.path.dirname(__file__), "admins.join")
+        with open(ruta, "w", encoding="utf-8") as f:
+            json.dump(admins, f, indent=4, ensure_ascii=False)
+
+        print(f"¡Bienvenido {usuario}! Administrador registrado correctamente")
+        return True
+    
+    except Exception as e:
+        print(f"Error: {e}")
+        return False
+
 def login_admin_menu():
     cargar_datos()
 
@@ -87,3 +152,10 @@ def login_admin_menu():
                     opcion_error = input("Seleccione: ").strip()
                     if opcion_error == "-1":
                         bandera = False
+
+        elif opcion == "0":
+            break
+
+        else:
+            print("Opcion invalida")
+                
